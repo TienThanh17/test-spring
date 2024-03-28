f80661789e7faa8564f8cc56c75580a6edfe73aa
meoKibo <theboyhunter123@gmail.com>
2024-03-29 00:32:00 +0700

    test

---
+++ demo/src/main/java/com/crud/demo/model/Employee.java
@@ -0,0 +1,20 @@
+package com.crud.demo.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "address")
+    private String address;
+}
